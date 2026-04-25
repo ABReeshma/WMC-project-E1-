9f148023e2bc8146e4926cc18298fd8f20a32c82
Reeshma <abreeshma6002@gmail.com>
2026-04-25 14:50:54 +0530

    Update taeftr-routing1.cc

--- taeftr-routing1.cc
+++ taeftr-routing1.cc
@@ -126,7 +126,7 @@ int main(int argc, char *argv[])
 
     // ---------------- SIMULATION ----------------
 
-    AnimationInterface anim("taeftr.xml");
+    AnimationInterface anim("taeftr1.xml");
     anim.EnablePacketMetadata(true);
     Simulator::Stop(Seconds(simTime));
     Simulator::Run();
